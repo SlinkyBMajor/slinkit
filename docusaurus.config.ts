b4100509721dd1a821ef6306f00ac3969cb9f12e
Pontus Rheindorf <pontus.rhe@gmail.com>
2024-05-23 10:56:53 +0200

    updated base url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: "https://slinkybmajor.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/slinkit",
   trailingSlash: false,
 
   // GitHub pages deployment config.
